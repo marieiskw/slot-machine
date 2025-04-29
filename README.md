019d8ef6fb351223fa3d969cb94f26bf2abd6ccc
marieiskw <git@stackblitz.com>
2025-04-29 16:52:42 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# slot-machine
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/marieiskw/slot-machine)
---
+++ src/slot.js
@@ -0,0 +1,42 @@
+const slots = [
+  document.getElementById('slot1'),
+  document.getElementById('slot2'),
+  document.getElementById('slot3'),
+];
+let isSpinning = [false, false, false];
+let slotTimers = [];
+
+const firstSymbols = ['0', '1', '2'];
+const otherSymbols = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+function startSlot() {
+  for (let i = 0; i < slots.length; i++) {
+    if (!isSpinning[i]) {
+      isSpinning[i] = true;
+      slotTimers[i] = setInterval(() => {
+        slots[i].textContent =
+          i === 0
+            ? firstSymbols[Math.floor(Math.random() * firstSymbols.length)]
+            : otherSymbols[Math.floor(Math.random() * otherSymbols.length)];
+      }, 40);
+    }
+  }
+}
+
+function stopSlot(reelIndex) {
+  if (isSpinning[reelIndex]) {
+    clearInterval(slotTimers[reelIndex]);
+    isSpinning[reelIndex] = false;
+  }
+}
+
+document.getElementById('startButton').addEventListener('click', startSlot);
+document
+  .getElementById('stopButton1')
+  .addEventListener('click', () => stopSlot(0));
+document
+  .getElementById('stopButton2')
+  .addEventListener('click', () => stopSlot(1));
+document
+  .getElementById('stopButton3')
+  .addEventListener('click', () => stopSlot(2));
